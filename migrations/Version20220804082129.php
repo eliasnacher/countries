d0ebab0951159df2e5c493d3c197ee7a826dee73
Elias Nacher <eliasnacher@gmail.com>
2022-08-04 11:03:42 +0200

    Add Country and Region entities and migrations

---
+++ migrations/Version20220804082129.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220804082129 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('CREATE TABLE country (id INT AUTO_INCREMENT NOT NULL, region_id INT NOT NULL, common_name VARCHAR(255) NOT NULL, official_name VARCHAR(255) NOT NULL, flag VARCHAR(255) NOT NULL, population INT NOT NULL, INDEX IDX_5373C96698260155 (region_id), PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('CREATE TABLE region (id INT AUTO_INCREMENT NOT NULL, name VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE `utf8mb4_unicode_ci` ENGINE = InnoDB');
+        $this->addSql('ALTER TABLE country ADD CONSTRAINT FK_5373C96698260155 FOREIGN KEY (region_id) REFERENCES region (id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE country DROP FOREIGN KEY FK_5373C96698260155');
+        $this->addSql('DROP TABLE country');
+        $this->addSql('DROP TABLE region');
+    }
+}
---
+++ src/Entity/Country.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CountryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: CountryRepository::class)]
+class Country
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $commonName = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $officialName = null;
+
+    #[ORM\ManyToOne(inversedBy: 'countries')]
+    #[ORM\JoinColumn(nullable: false)]
+    private ?Region $region = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $flag = null;
+
+    #[ORM\Column]
+    private ?int $population = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCommonName(): ?string
+    {
+        return $this->commonName;
+    }
+
+    public function setCommonName(string $commonName): self
+    {
+        $this->commonName = $commonName;
+
+        return $this;
+    }
+
+    public function getOfficialName(): ?string
+    {
+        return $this->officialName;
+    }
+
+    public function setOfficialName(string $officialName): self
+    {
+        $this->officialName = $officialName;
+
+        return $this;
+    }
+
+    public function getRegion(): ?Region
+    {
+        return $this->region;
+    }
+
+    public function setRegion(?Region $region): self
+    {
+        $this->region = $region;
+
+        return $this;
+    }
+
+    public function getFlag(): ?string
+    {
+        return $this->flag;
+    }
+
+    public function setFlag(string $flag): self
+    {
+        $this->flag = $flag;
+
+        return $this;
+    }
+
+    public function getPopulation(): ?int
+    {
+        return $this->population;
+    }
+
+    public function setPopulation(int $population): self
+    {
+        $this->population = $population;
+
+        return $this;
+    }
+}
